4c38d92e9c01ac0b0174ea5f21810413a631fc07
Furrioxx <pelud.tom1@gmail.com>
2025-02-18 16:40:09 +0100

    add user information to navbar

--- src/core/BaseController.php
+++ src/core/BaseController.php
@@ -2,19 +2,21 @@
 
 namespace Appy\Src\Core;
 
-class BaseController{
-    
+class BaseController
+{
+
     protected $loader;
     protected $twig;
 
-    public function __construct() {
-        $this->loader = new \Twig\Loader\FilesystemLoader(BASE_PATH .'src/template');
+    public function __construct()
+    {
+        $this->loader = new \Twig\Loader\FilesystemLoader(BASE_PATH . 'src/template');
         $this->twig = new \Twig\Environment($this->loader);
         $assetFunction = new \Twig\TwigFunction('asset', function ($path) {
-            echo WEB_PATH . "assets/" . $path ;
+            echo WEB_PATH . "assets/" . $path;
         });
         $webpathFucntion = new \Twig\TwigFunction('path', function ($path) {
-            echo WEB_PATH. $path;
+            echo WEB_PATH . $path;
         });
         $this->twig->addFunction($assetFunction);
         $this->twig->addFunction($webpathFucntion);
@@ -22,13 +24,15 @@ class BaseController{
 
     public function render(string $tempalteName, array $params = null)
     {
-        $params["logo"] = WEB_PATH ."/assets/img/logo.png";
+        $params["logo"] = WEB_PATH . "/assets/img/logo.png";
         $params["flash"] = Session::getInstance()::getFlashes();
+        $params["user"] = Appy::getAuth()->user();
         echo $this->twig->render($tempalteName, $params);
     }
 
-    public function redirectTo($path){
-        header("Location: ". WEB_PATH . $path);
+    public function redirectTo($path)
+    {
+        header("Location: " . WEB_PATH . $path);
         exit();
     }
 }
